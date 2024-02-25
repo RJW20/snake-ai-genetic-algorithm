fe00bc706d3e5578ebef0b3b71cf9974a68ba252
Robert Watts <robertjwatts84@outlook.com>
2024-02-25 16:47:32 +0000

    Add playbackpopulation class

--- snake_ai/playback.py
+++ snake_ai/playback.py
@@ -9,6 +9,7 @@ from .settings import playback_settings
 
 
 ##bind settings to variables
+population_size = genetic_algorithm_settings['population_settings']
 total_generations = genetic_algorithm_settings['total_generations']
 history_folder = genetic_algorithm_settings['history_folder']
 history_type = genetic_algorithm_settings['history_type']
@@ -33,12 +34,9 @@ def playback() -> None:
     pygame.display.set_caption("Snake")
     clock = pygame.time.Clock()
     running = True
-    dt = 0
 
-    #intialize the population
-
-    #load gen 1's history
-
+    #intialize the playback population
+    playback_pop = PlaybackPopulation(history_folder, history_type, history_value, population_size, player_args)
 
     while running:
 
@@ -58,3 +56,50 @@ def playback() -> None:
         #draw the snakes
 
     pygame.quit()
+
+
+class PlaybackPopulation(Population):
+    """Extension of Population class with methods allowing us to manipulate
+    which genomes are loaded.
+
+    Will determine its own size and automatically load the first genomes.
+    """
+
+    def __init__(self, 
+                 history_folder: str,
+                 history_type: str, 
+                 history_value: int, 
+                 og_pop_size: int, 
+                 player_args: dict
+                 ) -> None:
+        
+        self.folder = history_folder
+        self.current_generation = 1
+
+        self.is_champs = False
+        pop_size = og_pop_size
+        match(history_type):
+            case('none'):
+                raise Exception('No history was saved during evolution. If you would like ' + \
+                                'to view playback, please adjust history settings and run again.')
+            case('champ'):
+                pop_size = 1
+                self.is_champs = True
+            case('absolute'):
+                pop_size = history_value
+            case('percentage'):
+                pop_size = int(og_pop_size * history_value)
+            case _:
+                raise Exception(f'Invalid history type {history_type}')
+        self.size = pop_size
+        self.players = [Player(**player_args) for _ in range(pop_size)]
+
+        if self.is_champs:
+            self.load(history_folder)
+        else:
+            self.new_gen()
+
+    def new_gen(self) -> None:
+        """Load generation {self.gen_id}."""
+
+        self.load(f'{history_folder}/{self.current_generation}')
