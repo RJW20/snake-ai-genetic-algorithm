4d8ddc5e10a060c0208ff5369ac150a83e769f24
Robert Watts <robertjwatts84@outlook.com>
2024-02-26 11:58:33 +0000

    Change multiprocessing pool to use default chunksize

--- snake_ai/main.py
+++ snake_ai/main.py
@@ -42,7 +42,7 @@ def main() -> None:
 
         #run the players with multiprocessing
         with Pool(2) as pool:
-            population.players = pool.map(simulate, population.players, chunksize=1)
+            population.players = pool.map(simulate, population.players, chunksize=None)
 
         #print some stats
         print(f'\ngeneration: {population.current_generation}, champ\'s best score: {population.champ.best_score}, ' + 
